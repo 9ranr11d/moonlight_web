9d25b982e1a0a7f84d49e5f967e819db7bb7d587
minseob <min99seob@gmail.com>
2024-06-22 15:33:08 +0900

    'utils': 주석 추가

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -7,21 +7,17 @@ import { signOut } from "@redux/slices/AuthSlice";
  * @returns mm:ss
  */
 export const convertToMinutes = (time: number): string => {
+  /** 분 */
   const minute = Math.floor(time / 60);
+  /** 초 */
   const second = time % 60;
 
+  /** 형식 변경된 분 */
   const paddedMinutes = String(minute).padStart(2, "0");
+  /** 형식 변경된 초 */
   const paddedSecond = String(second).padStart(2, "0");
 
   return `${paddedMinutes}:${paddedSecond}`;
-};
-
-export const convertDateII = (date: Date, splitter: string): string => {
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
-
-  return convertDateI(year, month, day, splitter);
 };
 
 /**
@@ -33,15 +29,42 @@ export const convertDateII = (date: Date, splitter: string): string => {
  * @returns YYYY MM DD
  */
 export const convertDateI = (year: number, month: number, day: number, splitter: string): string => {
+  /** 형식 변경된 달 */
   const paddedMonth = String(month).padStart(2, "0");
+  /** 형식 변경된 일 */
   const paddedDay = String(day).padStart(2, "0");
 
   return `${year}${splitter}${paddedMonth}${splitter}${paddedDay}`;
 };
 
+/**
+ * 날짜 형식 설정
+ * @param date 날짜
+ * @param splitter 날짜 사이 나눌 기호
+ * @returns YYYY MM DD
+ */
+export const convertDateII = (date: Date, splitter: string): string => {
+  /** 연도 */
+  const year = date.getFullYear();
+  /** 달 */
+  const month = date.getMonth() + 1;
+  /** 일 */
+  const day = date.getDate();
+
+  return convertDateI(year, month, day, splitter);
+};
+
+/**
+ * 로그아웃
+ * @param confirmDesc 띄울 확인 메세지
+ * @param dispatch dispatch
+ * @returns 로그아웃 여부
+ */
 export const processSignOut = (confirmDesc: string, dispatch: AppDispatch): boolean => {
+  /** 확인 메세지 */
   const confirmSignOut: boolean = window.confirm(confirmDesc);
 
+  // 사용자가 취소 누를 시
   if (!confirmSignOut) return false;
 
   fetch("/api/auth/sign_out", { method: "POST" })
